a4a1d4c8094663585a5e8d9e08b52164cbadf267
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2019-08-06 12:31:20 +0900

    bundle gem uri

---
+++ lib/uri/version.rb
@@ -0,0 +1,3 @@
+module Uri
+  VERSION = "0.1.0"
+end
---
+++ test/uri_test.rb
@@ -0,0 +1,11 @@
+require "test_helper"
+
+class UriTest < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::Uri::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
